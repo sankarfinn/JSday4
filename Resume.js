6e5e6404fd4b2e0cd7faf0236f205380c3bd0639
sankarfinn <sankar113073@gmail.com>
2024-07-01 21:14:43 +0530

    first commit

---
+++ Resume.js
@@ -0,0 +1,49 @@
+[
+  {
+    personal_details: {
+      name: "sankar",
+      address: "no73,temple streer,aruppukottai",
+      phone: "8248574256",
+      email: "sankar113073@gmail.com",
+      linkedin: "https://",
+    },
+    education: [
+      {
+        institution: "sri,sowdambika collage",
+        location: "aruppukottai",
+        degree: "Diploma(Electrical and electronics engineering)",
+        year: "2013",
+        percentage: "73",
+      },
+      {
+        institution: "devangar higher secondary school",
+        location: "aruppukottai",
+        degree: "class X",
+        year: "2010",
+        percentage: "63",
+      },
+    ],
+    experience: [
+      {
+        job_title: "junior engineer",
+        company: "igarashi motor ind ltd",
+        location: "mepz chennai",
+        "start date": "october2022",
+        "end date": "till now",
+        responsibilities: ["machine maintenance and trobleshooting"],
+      },
+    ],
+    skills: [
+      "web development : HTML,CSS,JavaScript",
+      "Database management : SQL, MongoDB",
+      "Version control : git",
+    ],
+    certifications: [
+      {
+        name: "MERN Stack Developer",
+        issuing_organization: "GUVI",
+        issue_date: "still now",
+      },
+    ],
+  },
+];
